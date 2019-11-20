6bbd40254df93066644642829aa3ac7eb76dbf83
zhangpanpan <zjrzpp0414@163.com>
2019-11-20 09:03:37 +0800

    删除node_modules,dist

--- dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    // curd 就是数据库名字
-    connectionStr: 'mongodb+srv://zpp:zpp123456@serverapi-rb13v.mongodb.net/curd?retryWrites=true&w=majority'
-};
